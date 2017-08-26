f3d66a4b5407ead55498b9ffd9a0e8b76f07dadf
Rajpeer <31372812+RajCollection@users.noreply.github.com>
2017-08-26 23:26:23 +0530

    client file created

---
+++ client.js
@@ -0,0 +1,17 @@
+var mqtt = require('mqtt');  //npm install mqtt
+var url = "mqtt://127.0.0.1"; //you can use personal server url or ip address
+
+var options = {
+  port: 1883,
+  clientId: 'mqtjs_' + Math.random().toString(16).substr(2, 8)
+};
+
+//if run in local machine with port number 1883
+//client = mqtt.connect({port:1883});
+
+//if run in remote machine or server
+client = mqtt.connect(url,options);
+
+//publish or send message
+client.publish("myTopicNameHere","Hello MQTT");
+
